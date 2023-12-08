5507e34d0ff50e0e949317e7d528e870e3ce9c04
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-08 19:08:25 +0900

    feat: set PATH on bash

--- private_dot_config/private_fish/conf.d/abbr.fish
+++
@@ -1,5 +0,0 @@
-abbr --add --global -- g git
-abbr --add --global -- gl 'git log --oneline'
-abbr --add --global -- k kubectl
-abbr --add --global -- a aws
-abbr --add --global -- te terraform
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,19 +1,48 @@
-bind \cg '__ghq_look'
+# if interactive shell
+if not status is-interactive
+    return
+end
+
+# fnm (node.js)
+if command -q fnm;
+    fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled | source; \
+        or echo "fnm initialization failed"
+else
+    echo "fnm: not found"
+end
+
+# opam
+if [ -f $HOME/.opam/opam-init/init.fish ];
+    source $HOME/.opam/opam-init/init.fish; or echo "opam initialization failed"
+else
+    echo "opam: not found"
+end
 
 # starship
-if which starship > /dev/null
-  starship init fish | source
+if command -q starship;
+    starship init fish | source; or echo "starship initialization failed"
+else
+    echo "starship: not found"
 end
 
-# direnv hook
-if which direnv > /dev/null
-  eval (direnv hook fish)
+# direnv
+if command -q direnv;
+    direnv hook fish | source; or echo "direnv: initialization failed"
+else
+    echo "direnv: not found"
 end
 
-# opam configuration
-# source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+# key bindings
+bind \cg '__ghq_look'
 
-# fnm configuration
-if which fnm > /dev/null
-  fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled | source; or true
-end
+# abbrs
+# Git
+abbr --add --global -- g git
+abbr --add --global -- gl 'git log --oneline'
+abbr --add --global -- gc 'git commit'
+abbr --add --global -- gp 'git push'
+abbr --add --global -- gpf 'git push --force-with-lease'
+
+abbr --add --global -- k kubectl
+abbr --add --global -- a aws
+abbr --add --global -- te terraform
