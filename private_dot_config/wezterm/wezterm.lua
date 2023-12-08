f80d43655748767c132b1352fd53c1226251f201
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-09 02:42:24 +0900

    feat: split/select/swap panes

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,10 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
 
 config.color_scheme = 'iceberg-dark'
 config.font =
@@ -7,11 +12,10 @@ config.font =
 config.font_size = 13
 
 config.keys = {
-    {
-        key = ']',
-        mods = 'CTRL',
-        action = wezterm.action.PaneSelect
-    },
+    { key = '[', mods = 'CTRL', action = act.PaneSelect },
+    { key = ']', mods = 'CTRL', action = act.PaneSelect { mode = 'SwapWithActive' }, },
+    { key = '-', mods = 'CTRL|ALT', action = act.SplitPane { direction = 'Down' }, },
+    { key = '\\',mods = 'CTRL|ALT', action = act.SplitPane { direction = 'Right' }, },
 }
 
 return config
