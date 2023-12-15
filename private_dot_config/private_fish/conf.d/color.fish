814adb8b0a3571e435c9fd7c82a3d8e0f2ca3ecc
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-15 13:04:05 +0900

    feat: color settings for fish shell

---
+++ private_dot_config/private_fish/conf.d/color.fish
@@ -0,0 +1,14 @@
+set -g fish_color_normal normal
+set -g fish_color_command blue
+set -g fish_color_keyword blue --bold
+set -g fish_color_quote yellow
+set -g fish_color_redirection magenta --bold
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_param cyan
+set -g fish_color_valid_path cyan --underline
+set -g fish_color_option cyan --bold
+set -g fish_color_comment red
+set -g fish_color_operator brcyan
+set -g fish_color_escape brblue
+set -g fish_color_autosuggestion brblack
