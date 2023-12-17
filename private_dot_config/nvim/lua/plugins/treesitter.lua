347fa5cc154243eaf0b53a63ab5bada63284b9bb
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-17 14:11:27 +0900

    refactor(nvim/treesitter): reorder dependencies

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,60 +1,6 @@
-function setup_treesitter()
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.satysfi = {
-        install_info = {
-            url = 'https://github.com/monaqa/tree-sitter-satysfi.git',
-            files = { 'src/parser.c', 'src/scanner.c' },
-        }, 
-        filetype = 'satysfi',
-    }
-
-    require('nvim-treesitter.configs').setup {
-        -- なくて困ったら入れればよい
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = 'gnn',
-                node_incremental = 'grn',
-                scope_incremental = 'grc',
-                node_decremental = 'grm',
-            },
-        },
-    }
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-end
-
-function setup_rainbow_delimiters()
-    local rd = require 'rainbow-delimiters'
-    require('rainbow-delimiters.setup').setup {
-        strategy = {
-            [''] = rd.strategy['global'],
-            vim = rd.strategy['local'],
-        },
-        query = {
-            [''] = 'rainbow-delimiters',
-            lua = 'rainbow-blocks',
-        },
-        highlight = {
-            'RainbowDelimiter1',
-            'RainbowDelimiter2',
-            'RainbowDelimiter3',
-            'RainbowDelimiter4',
-            'RainbowDelimiter5',
-        },
-    }
-end
-
-return {
+local treesitter = {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        {
-            'HiPhish/rainbow-delimiters.nvim',
-            config = setup_rainbow_delimiters,
-        }
-    },
+    dependencies = {},
     event = { 'BufReadPost', 'BufNewFile' },
     build = ':TSUpdate',
     cmd = { 'TSUpdateSync' },
@@ -62,6 +8,58 @@ return {
         return not vim.g.vscode
     end,
     config = function()
-        setup_treesitter()
+        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+        parser_config.satysfi = {
+            install_info = {
+                url = 'https://github.com/monaqa/tree-sitter-satysfi.git',
+                files = { 'src/parser.c', 'src/scanner.c' },
+            }, 
+            filetype = 'satysfi',
+        }
+
+        require('nvim-treesitter.configs').setup {
+            -- なくて困ったら入れればよい
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = 'gnn',
+                    node_incremental = 'grn',
+                    scope_incremental = 'grc',
+                    node_decremental = 'grm',
+                },
+            },
+        }
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
     end,
+}
+
+table.insert(treesitter.dependencies, {
+    'HiPhish/rainbow-delimiters.nvim',
+    config = function()
+        local rd = require 'rainbow-delimiters'
+        require('rainbow-delimiters.setup').setup {
+            strategy = {
+                [''] = rd.strategy['global'],
+                vim = rd.strategy['local'],
+            },
+            query = {
+                [''] = 'rainbow-delimiters',
+                lua = 'rainbow-blocks',
+            },
+            highlight = {
+                'RainbowDelimiter1',
+                'RainbowDelimiter2',
+                'RainbowDelimiter3',
+                'RainbowDelimiter4',
+                'RainbowDelimiter5',
+            },
+        }
+    end
+})
+
+return {
+    treesitter,
 }
