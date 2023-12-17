8e4640152a2f5e03d222727e069e02d2de14619d
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-17 14:16:20 +0900

    feat(nvim/treesitter): add nvim-treesitter-context

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -37,6 +37,24 @@ local treesitter = {
 }
 
 table.insert(treesitter.dependencies, {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+        require('treesitter-context').setup {
+            enable = true,
+            max_lines = 0, -- unlimited
+            min_window_height = 20,
+            line_numbers = true,
+            multiline_threshold = 10,
+            trim_scope = 'outer',
+            mode = 'cursor',
+            separator = nil,
+            zindex = 20,
+            on_attach = nil,
+        }
+    end
+})
+
+table.insert(treesitter.dependencies, {
     'HiPhish/rainbow-delimiters.nvim',
     config = function()
         local rd = require 'rainbow-delimiters'
