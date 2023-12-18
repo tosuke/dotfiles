15e26ff0cba582d8e41612d69b05650b12d17d23
tosuke <13393900+tosuke@users.noreply.github.com>
2023-12-19 03:37:10 +0900

    fix(cmp): complete only selected entry

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -246,7 +246,10 @@ local plugins = {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = false
+                    },
                 },
                 sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
