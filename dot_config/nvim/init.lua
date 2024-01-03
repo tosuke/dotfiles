491019f6f982bc832a978b76b98db363e40301b8
tosuke <13393900+tosuke@users.noreply.github.com>
2024-01-04 02:55:38 +0900

    feat(nvim): manage node tools

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -99,7 +99,11 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('plugins', {
+require('lazy').setup({
+    spec = {
+        { import = 'plugins' },
+        { import = 'tools' },
+    },
     defaults = {
         lazy = true,
     },
