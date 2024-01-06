6c875d200cc54bde6f75d27a0881100ceec6e6a5
tosuke <13393900+tosuke@users.noreply.github.com>
2024-01-06 11:21:50 +0900

    feat: add ocaml rainbow delimiters scheme

---
+++ dot_config/nvim/after/queries/ocaml/rainbow-delimiters.scm
@@ -0,0 +1,83 @@
+(type_binding
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(constructed_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(parenthesized_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(constrain_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(abstract_type
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(module_parameter
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(parenthesized_module_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(parenthesized_operator
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(parenthesized_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(record_declaration
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(typed_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(array_expression
+  "[|" @delimiter
+  "|]" @delimiter @sentinel) @container
+
+(record_expression
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(array_get_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(extension
+  "[%" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(quoted_string
+  "{" @delimiter
+  "}" @delimiter @sentinel) @container
+
+(string_get_expression
+  "[" @delimiter
+  "]" @delimiter @sentinel) @container
+
+(parenthesized_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(package_pattern
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(package_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
+
+(local_open_expression
+  "(" @delimiter
+  ")" @delimiter @sentinel) @container
