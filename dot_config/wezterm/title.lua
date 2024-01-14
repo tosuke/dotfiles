58977e266ceef699213050a590ce00b6b0bb1da8
tosuke <13393900+tosuke@users.noreply.github.com>
2024-01-15 03:23:13 +0900

    feat(nvim): muxed tab view

--- dot_config/wezterm/title.lua
+++ dot_config/wezterm/title.lua
@@ -6,6 +6,7 @@ local function basename(s)
 end
 
 local HEADER_ICON_DEFAULT = ""
+local HEADER_ICON_REMOTE = wezterm.nerdfonts.cod_remote
 local HEADER_ICONS = {
     -- editor
     nvim = wezterm.nerdfonts.custom_vim,
@@ -34,20 +35,33 @@ local HOVER_COLOR = "#111111"
 
 wezterm.on("format-tab-title", function(tab, _, _, _, hover, _)
     local active = tab.is_active and 1 or 2
-    local process = tab.active_pane.foreground_process_name
+
+    local active_pane_info = tab.active_pane
+    local active_pane = wezterm.mux.get_pane(active_pane_info.pane_id)
+    local domain_name = active_pane:get_domain_name()
+    local is_remote = domain_name ~= "local"
+
+    local process = active_pane_info.foreground_process_name
     local process_name = process and basename(process) or ""
-    local cwd_url = tab.active_pane.current_working_dir
-    local cwd = cwd_url and basename(cwd_url.file_path)
+    local cwd_url = active_pane_info.current_working_dir
+    local cwd = cwd_url and basename(cwd_url)
 
     local bg = hover and HOVER_COLOR or BACK_COLOR[active]
 
     local index = tab.tab_index + 1
-    local zoomed = tab.active_pane.is_zoomed and "🔎 " or " "
-    local icon = HEADER_ICONS[process_name] or HEADER_ICON_DEFAULT
+    local zoomed = active_pane_info.is_zoomed and "🔎 " or " "
+    local default_icon = is_remote and HEADER_ICON_REMOTE or HEADER_ICON_DEFAULT
+    local icon = HEADER_ICONS[process_name] or default_icon
 
     local title = tab.active_pane.title
     if cwd then
-        title = cwd .. " | " .. process_name
+        title = cwd
+        if process_name ~= "" then
+            title = title .. " | " .. process_name
+        end
+        if is_remote then
+            title = title .. " - " .. domain_name
+        end
     end
 
     return {
