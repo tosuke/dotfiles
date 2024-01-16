970dc3eb6bbd1d27c77e150054be5bca14a68ef8
tosuke <13393900+tosuke@users.noreply.github.com>
2024-01-16 17:21:42 +0900

    feat(nvim): tab completion

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -439,6 +439,15 @@ local plugins = {
         config = function()
             local cmp = require("cmp")
 
+            local function has_word_before()
+                if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+                    return false
+                end
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+            end
+
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -449,6 +458,13 @@ local plugins = {
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                        if cmp.visible() and has_word_before() then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        else
+                            fallback()
+                        end
+                    end),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({
                         behavior = cmp.ConfirmBehavior.Replace,
@@ -684,6 +700,11 @@ local plugins = {
                 },
                 settings = {
                     Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                            pathStrict = true,
+                            path = { "?.lua", "?/init.lua" },
+                        },
                         diagnostics = {
                             globals = { "vim" },
                         },
